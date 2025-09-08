54512304bc8565d7c9762c8004ba3d9b004331d0
Krendel32 <krendel32uwu@gmail.com>
2025-09-08 11:56:45 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,67 @@
+
+const schedule = {
+  even: {
+    monday: [
+      { subject: " - ", start: "08:00", end: "9:20" },
+      { subject: "Л Історія та культура", start: "9:35", end: "10:55" },
+      { subject: "Л Сучасна українська мова", start: "11:10", end: "12:30" },
+      { subject: " - ", start: "12:45", end: "14:05"}
+    ],
+    tuesday: [
+      { subject: " - ", start: "08:00", end: "9:20" },
+      { subject: "Пр Фізичне виховання", start: "9:35", end: "10:55" },
+      { subject: "Л Вища математика", start: "11:10", end: "12:30" },
+      { subject: " - ", start: "12:45", end: "14:05"}
+    ],
+    wednesday: [
+      { subject: "Пр Менеджмент та підприємництво", start: "08:00", end: "9:20" },
+      { subject: "Пр Сучасна українська мова", start: "9:35", end: "10:55" },
+      { subject: "Лз Основи програмування", start: "11:10", end: "12:30" },
+      { subject: " - ", start: "12:45", end: "14:05"}
+    ],
+    thursday: [
+      { subject: "Пр Фізика", start: "08:00", end: "9:20" },
+      { subject: "Пр Вища математика", start: "9:35", end: "10:55" },
+      { subject: "Л Основи програмування", start: "11:10", end: "12:30" },
+      { subject: "- ", start: "12:45", end: "14:05"}
+    ],
+    friday: [
+      { subject: " - ", start: "08:00", end: "9:20" },
+      { subject: "Л Автоматизація інженерних розрахунків", start: "9:35", end: "10:55" },
+      { subject: "Пр Автоматизація інженерших розрахунків", start: "11:10", end: "12:30" },
+      { subject: " - ", start: "12:45", end: "14:05"}
+    ],
+  },
+  odd: {
+    monday: [
+      { subject: "Пр Вступ до спеціальності", start: "08:00", end: "9:20" },
+      { subject: "Л Історія та культура", start: "9:35", end: "10:55" },
+      { subject: "Пр Фізика", start: "11:10", end: "12:30" },
+      { subject: " - ", start: "12:45", end: "14:05"}
+    ],
+    tuesday: [
+      { subject: " - ", start: "08:00", end: "9:20" },
+      { subject: "Пр Фізичне виховання", start: "9:35", end: "10:55" },
+      { subject: "Л Фізика", start: "11:10", end: "12:30" },
+      { subject: " - ", start: "12:45", end: "14:05"}
+    ],
+    wednesday: [
+      { subject: " - ", start: "08:00", end: "9:20" },
+      { subject: "Лз Основи програмування", start: "9:35", end: "10:55" },
+      { subject: "Пр Історія та культура", start: "11:10", end: "12:30" },
+      { subject: " - ", start: "12:45", end: "14:05"}
+    ],
+    thursday: [
+      { subject: "Л Вища математика", start: "08:00", end: "9:20" },
+      { subject: "Л Вступ до спеціальності", start: "9:35", end: "10:55" },
+      { subject: "Л Основи програмування", start: "11:10", end: "12:30" },
+      { subject: "Пр Вступ до спеціальності", start: "12:45", end: "14:05"}
+    ],
+    friday: [
+      { subject: " - ", start: "08:00", end: "9:20" },
+      { subject: "Пр Автоматизація інженерних розрахунків", start: "9:35", end: "10:55" },
+      { subject: "Л Менеджмент та підприємництво", start: "11:10", end: "12:30" },
+      { subject: " - ", start: "12:45", end: "14:05"}
+    ],
+  }
+};
