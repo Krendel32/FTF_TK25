ff0289ef017fb6e37bea1e46315ddffd3cc941ea
Krendel32 <krendel32uwu@gmail.com>
2025-09-10 09:32:01 +0300

    Update data.js

--- data.js
+++ data.js
@@ -16,7 +16,7 @@ const schedule = {
     wednesday: [
       { subject: " - ", start: "08:00", end: "9:20" },
       { subject: "Пр Сучасна українська мова", start: "9:35", end: "10:55" },
-      { subject: "Лз Основи програмування", start: "11:10", end: "12:30" },
+      { subject: "Лз Основи програмування  з 26.11, 10.12", start: "11:10", end: "12:30" },
       { subject: " - ", start: "12:45", end: "14:05"}
     ],
     thursday: [
@@ -34,7 +34,7 @@ const schedule = {
   },
   odd: {
     monday: [
-      { subject: "Пр Вступ до спеціальності", start: "08:00", end: "9:20" },
+      { subject: "Пр Вступ до спеціальності   з 03.11", start: "08:00", end: "9:20" },
       { subject: "Л Історія та культура", start: "9:35", end: "10:55" },
       { subject: "Пр Фізика", start: "11:10", end: "12:30" },
       { subject: " - ", start: "12:45", end: "14:05"}
